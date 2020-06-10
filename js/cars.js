c5df92ce86b6f59bc99eeb8f954808ed4e49618c
Mickael Corroyer <mickael.corroyer@telecom-paristech.fr>
2020-06-11 01:47:06 +0200

    scatter plot, tooltip

--- js/cars.js
+++ js/cars.js
@@ -1,16 +1,24 @@
-const w = 600;
-const h = 600;
+const margin = {top: 20, right: 20, bottom: 40, left: 40};
+const canvasWidth = 600 - margin.left - margin.right;
+const canvasHeight = 600 - margin.top - margin.bottom;
 let dataset = [];
 
-// Create SVG canvas
+// create SVG canvas
 let svg = d3.select("body")
             .append("svg")
             .attr("id", "canvas")
-            .attr("width", w)
-            .attr("height", h);
+            .attr("width", canvasWidth + margin.left + margin.right)
+            .attr("height", canvasHeight + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.text('data/cars.csv', (error, raw) => {
-    let dsv = d3.dsvFormat(';')
+// text area for the tooltip
+let tooltip = d3.select("body")
+                .append("div");
+
+
+d3.text('data/processed_cars.csv', (error, raw) => {
+    let dsv = d3.dsvFormat(',')
     dataset = dsv.parse(raw, (d) => {
         return {
             car: d.Car,
@@ -28,10 +36,78 @@ d3.text('data/cars.csv', (error, raw) => {
 
     console.log("Loaded " + dataset.length + " rows.");
 
+    if (dataset.length > 0) {
+        // test data loading
+        console.log("First row: ", dataset[0]);
+        console.log("Second row: ", dataset[1]);
+        console.log("Last row: ", dataset[dataset.length - 1]);
+
+        // scales
+        xValue = function(d) { return d.acceleration; };
+        yValue = function(d) { return d.displacement; };
+
+        xScale = d3.scaleLinear()
+            // avoid data to overlap axis
+            .domain([d3.min(dataset, xValue)-2, d3.max(dataset, xValue)+2])
+            .range([0, canvasWidth]);
+        yScale = d3.scaleLinear()
+            .domain([d3.min(dataset, yValue)-2, d3.max(dataset, yValue)+2])
+            .range([canvasHeight, 0]);
+    }
+
     draw();
 })
 
 
 function draw() {
-    
+    // draw data as dots
+    svg.selectAll(".dot")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("class", "dot")
+    .attr("r", 3)
+    .attr("cx", (d) => xScale(xValue(d)))
+    .attr("cy", (d) => yScale(yValue(d)))
+    .attr("fill", "red")
+    .attr("stroke", "black")
+    // tooltip with smooth transitions when hovered
+    .on("mouseover", function(d) {
+        tooltip.transition()
+        .duration(300)
+        .style("opacity", .8);
+        tooltip.html("Model: " + d.car + "<br>Year: " + d.year + "<br>Origin: " + d.continent);
+    })
+    .on("mouseout", function(d) {
+        tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+    });
+
+    // x axis
+    svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0, " + canvasHeight + ")")
+    .call(d3.axisBottom(xScale))
+    // x axis label
+    svg.append("text")
+    .attr("class", "label")
+    .attr("x", canvasWidth/2)
+    .attr("y", canvasHeight + margin.bottom)
+    .attr("text-anchor", "middle")
+    .text("component 1");
+
+    // y axis
+    svg.append("g")
+    .attr("class", "y axis")
+    .call(d3.axisRight(yScale));
+    // y axis label
+    svg.append("text")
+    .attr("class", "label")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left)
+    .attr("x", -(canvasHeight/2))
+    .attr("dy", "1.5em")
+    .attr("text-anchor", "middle")
+    .text("component 2");
 }
