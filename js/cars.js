ff29d9624cb25206f2e728684b5827637ff36fc7
paulracaniere <racanierepaul@gmail.com>
2020-06-12 10:44:47 +0200

    add(CSVRead): include function to read specific columns of processed data

--- js/cars.js
+++ js/cars.js
@@ -11,6 +11,48 @@ let yScale;
 let colorValue;
 let colorScale;
 
+const engineSpecs = ["MPG", "Cylinders", "Displacement", "Horsepower", "Weight", "Acceleration"]
+let setOfEngineCharIndices = new Set([0, 1, 2, 3, 4, 5]);
+
+function CSVDataParser(listOfIndices) {
+    let k1 = "", k2 = "";
+    if (listOfIndices.length < 2) {
+        console.error("CSVDataParser: Should not have less than 2 values.");
+        return;
+    } else if (listOfIndices.length === 2) {
+        k1 = engineSpecs[listOfIndices[0]];
+        k2 = engineSpecs[listOfIndices[1]];
+    } else {
+        listOfIndices.sort();
+        k1 = "PC1_" + listOfIndices.join('');
+        k2 = "PC2_" + listOfIndices.join('');
+    }
+
+    return (d) => {
+        return {
+            car: d.Car,
+            mpg: +d.MPG,
+            cylinders: +d.Cylinders,
+            displacement: +d.Displacement,
+            hp: +d.Horsepower,
+            weight: +d.Weight,
+            acceleration: +d.Acceleration,
+            year: +d.Model,
+            continent: d.Origin,
+
+            mpg_norm: +d.MPG_norm,
+            cylinders_norm: +d.Cylinders_norm,
+            displacement_norm: +d.Displacement_norm,
+            hp_norm: +d.Horsepower_norm,
+            weight_norm: +d.Weight_norm,
+            acceleration_norm: +d.Acceleration_norm,
+
+            PC1: +d[k1],
+            PC2: +d[k2],
+        };
+    };
+}
+
 // create SVG canvas
 let svg = d3.select("body")
             .append("svg")
@@ -26,19 +68,7 @@ let tooltip = d3.select("body")
 
 
 d3.text('data/processed_cars.csv', (error, raw) => {
-    dataset = d3.csvParse(raw, (d) => {
-        return {
-            car: d.Car,
-            mpg: +d.MPG,
-            cylinders: +d.Cylinders,
-            displacement: +d.Displacement,
-            hp: +d.Horsepower,
-            weight: +d.Weight,
-            acceleration: +d.Acceleration,
-            year: +d.Model,
-            continent: d.Origin,
-        };
-    });
+    dataset = d3.csvParse(raw, CSVDataParser([...setOfEngineCharIndices]));
     console.log("Loaded " + dataset.length + " rows.");
 
     if (dataset.length > 0) {
