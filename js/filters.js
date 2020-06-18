dae9dc2e73e4a1f3be0b0868134029e6348d602b
Julien Eudine <julien.eudine@telecom-paris.fr>
2020-06-18 08:34:57 +0200

    wip

--- js/filters.js
+++ js/filters.js
@@ -35,54 +35,13 @@
 
     for(let k = 0 ; k<names.length; k++) {
 
-        const filter_div = filters_section.append("div");
-        filter_div.html("<h3>" + names[k] + "</h3>");
-        const svg = filter_div.append("svg")
-            .attr("id", "filters_canvas")
-            .attr("width","300")
-            .attr("height", "100");
-       const value_disp = filter_div.append("p");
-
-        const margin = {right: 50, left: 50},
-            width = +svg.attr("width") - margin.left - margin.right,
-            height = +svg.attr("height");
-
-        const x = d3.scaleLinear()
-            .domain([mins[k], maxs[k]])//depend on the attribute
-            .range([0, width])// size on the screen
-            .clamp(true);
-
-        const slider = svg.append("g")
-            .attr("class", "slider")
-            .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
-
-        slider.append("line")
-            .attr("class", "track")
-            .attr("x1", x.range()[0])
-            .attr("x2", x.range()[1])
-            .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-            .attr("class", "track-inset")
-            .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-            .attr("class", "track-overlay")
-            .call(d3.drag()
-                .on("start.interrupt", function() { slider.interrupt(); })
-                .on("start drag", function() { const val = x.invert(d3.event.x);
-                    handle.attr("cx", x(val));
-                    value_disp.html(String(Math.round(val)));
-                    handlers[k](val); }));
-
-        const handle = slider.insert("circle", ".track-overlay")
-            .attr("class", "handle")
-            .attr("r", 9)
-
-        slider.transition() // Gratuitous intro
-            .duration(750)//duration of the intro
-            .tween("hue", function() {
-                var i = d3.interpolate(0, (mins[k] + maxs[k])/2); //range
-                return function(t) {handle.attr("cx", x(i(t)));
-                    value_disp.html(String(Math.round(i(t))));
-                    handlers[k](i(t));
-                };
-            });
+        const filter_div = filters_section.append("div")
+            .attr("id", "slider-container-" +String(k));
+        const slider = createD3RangeSlider(0, 100, "#slider-container-" + String(k));
+        slider.range(1,10);
+        slider.onChange(function(newRange){
+            console.log(newRange);
+        });
     }
+
 }
