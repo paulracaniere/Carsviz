caecee9d4390905357f0e603611d038ef28444c6
Julien Eudine <julien.eudine@telecom-paris.fr>
2020-06-19 19:00:14 +0200

    done

--- js/filters.js
+++ js/filters.js
@@ -10,37 +10,45 @@
 
     const handlers = [
         function(h) {
-            console.log("MPG: " + h);
+            console.log("MPG: [" + h.begin + ", " + h.end + "]");
         },
         function(h) {
-            console.log("Cylinder: " + h);
+            console.log("Cylinder: [" + h.begin + ", " + h.end + "]");
         },
         function(h) {
-            console.log("Displacement: " + h);
+            console.log("Displacement: [" + h.begin + ", " + h.end + "]");
         },
         function(h) {
-            console.log("Horse power: " + h);
+            console.log("Horse power: [" + h.begin + ", " + h.end + "]");
         },
         function(h) {
-            console.log("Weight: " + h);
+            console.log("Weight: [" + h.begin + ", " + h.end + "]");
         },
         function(h) {
-            console.log("Acceleration: " + h);
+            console.log("Acceleration: [" + h.begin + ", " + h.end + "]");
         },
         function(h) {
-            console.log("Model: " + h);
+            console.log("Model: [" + h.begin + ", " + h.end + "]");
         }];
 
     const filters_section = d3.select("body").append("section")
 
     for(let k = 0 ; k<names.length; k++) {
+        const filter_div = filters_section.append("div").attr("class", "filter_div");
+        filter_div.html("<p> " + names[k] + " </p>");
 
-        const filter_div = filters_section.append("div")
-            .attr("id", "slider-container-" +String(k));
-        const slider = createD3RangeSlider(0, 100, "#slider-container-" + String(k));
-        slider.range(1,10);
-        slider.onChange(function(newRange){
-            console.log(newRange);
+
+        const slider_div = filter_div.append("div")
+            .attr("id", "slider-container-" +String(k))
+            .attr("class", "slider_m");
+
+        const value_disp = filter_div.append("p");
+        value_disp.html("[ " + mins[k] + ", " + maxs[k] + "]");
+        const slider = createD3RangeSlider(mins[k], maxs[k], "#slider-container-" + String(k));
+        slider.range(mins[k], maxs[k]); //initial values
+        slider.onChange(function(range) {
+            value_disp.html("[ " + range.begin + ", " + range.end + "]");
+            handlers[k](range); //handler
         });
     }
 
